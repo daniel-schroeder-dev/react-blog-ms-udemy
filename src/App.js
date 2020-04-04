2432aa84900a7f41045a1569f3fd06b8330a70b2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-03 23:09:04 -0500

    Render posts from fetch request.

--- src/App.js
+++ src/App.js
@@ -10,15 +10,61 @@ import './App.css';
 
 const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
 
+const authors = [
+  'Bob Worth',
+  'Daniel Schroeder',
+  'Ted Long',
+];
+
 class App extends React.Component {
 
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    fetch(postsUrl)
+      .then(response => response.json())
+      .then(posts => this.setupPosts(posts))
+      .catch(console.error);
+  }
+
+  setupPosts = posts => {
+    this.setState({ posts:
+      [
+        {
+          id: posts[0].id,
+          author: authors[0],
+          title: posts[0].title.substring(0, 8),
+          body: posts[0].body.substring(0, 100) + '...',
+          src: doctor,
+        },
+        {
+          id: posts[10].id,
+          author: authors[1],
+          title: posts[10].title.substring(0, 12),
+          body: posts[10].body.substring(0, 100) + '...',
+          src: cupcakes,
+        },
+        {
+          id: posts[20].id,
+          author: authors[2],
+          title: posts[20].title.substring(0, 5),
+          body: posts[20].body.substring(0, 100) + '...',
+          src: elf,
+        },
+      ]
+    });
+  };
+
   render() {
+    
     return (
       <main className="app">
         <div className="app__blog-card-container">
-          <BlogCard src={cupcakes} />
-          <BlogCard src={doctor} />
-          <BlogCard src={elf} />
+          { this.state.posts.map(post => (
+            <BlogCard key={post.id} {...post} />
+            ))}
         </div>
       </main>
     );  
--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types';
 
 import './BlogCard.css';
 
+// id: posts[20].id,
+// author: authors[2],
+// title: posts[20].title,
+// body: posts[20].body,
+// img: elf,
+
 function BlogCard(props) {
   return (
     <div className="blog-card">
       <img className="blog-card__img" src={props.src} alt="random unsplash" />
       <div className="blog-card__info">
-        <h2 className="blog-card__title">Blog Title</h2>
+        <h2 className="blog-card__title">{props.title}</h2>
         <h3 className="blog-card__category">Category</h3>
-        <p>The first part of the text of the blog...</p>
-        <p>Blog Author</p>
+        <p className="blog-card__body">{props.body}</p>
+        <p className="blog-card__author">{props.author}</p>
       </div>
     </div>    
   );
